2c76c9b42888a330c18e0e3527c4da06105f5762
piaoxiaobo <643209899@qq.com>
2018-04-13 19:26:25 +0800

    first commit

---
+++ datas/list-data.js
@@ -0,0 +1,164 @@
+let list_data = [
+  {
+
+    date: 'sep 19 2016',
+    title: '尚硅谷',
+    detail_img: '/images/detail/carousel/01.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content:'对你的感情，我自己也说不清楚。或许是依赖更多一点，又或许真的是爱情。见面就很开心，那种笑是发自内心的，无需多言，便很心安。不见，总是想念，想着你在哪儿，在干什么。每晚的一句“晚安”，不是情侣却胜似情侣。在你面前，我可以无忧无虑地像个孩子，可以撒娇，不必在乎别人的眼光，只要有你在身边就足够了。',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/detail/carousel/01.jpg',
+    author: '投影时代',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '并可以在手机上体验该小程序的实际效果。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '教育理念',
+    detail_img: '/images/detail/carousel/02.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '总是想要见你，却总是以失败告终。因为你太忙了，或许只是借口吧。我甚至后来都没有勇气要求见面，因为被拒绝了太多次。也许，我只是缺少一个见你的身份吧，我们的年龄，我们的职位，我们的家庭……各种问题，都是你我之间的阻碍。好不容易鼓起勇气给你打电话，却被无情挂断，你真的那么忙吗？我下定决心不再麻烦你了，你却说你有时候只是不想看到我那么无助，其实有你这句话足够了！当你主动问我过得是不是不太好，心酸的眼泪又会掉下来。',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/detail/carousel/02.jpg',
+    author: 'VR世家',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '并可以在手机上体验该小程序的实际效果。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 1
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '学科介绍',
+    detail_img: '/images/detail/carousel/03.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '开心的事情想和你一起分享，伤心的事情想和你倾诉，只需要你的倾听和安慰。从不奢求太多，只愿你永远幸福。你说我是水，我的性子是风，风起浪涌，风去水静。这个形容也是恰到好处，没有一点夸张。',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/detail/carousel/03.jpg',
+    author: 'VR世家',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '并可以在手机上体验该小程序的实际效果。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 2
+  },
+  {
+    date: '2018/3/15 下午 4:30:35',
+    title: '贾静雯简介',
+    detail_img: '/images/detail/list/04.jpg',
+    detail_content: '要走了，或许再无见面的机会，很不舍，只好在下一个路口等你，等你回头看到我，等你接受我的爱。今生注定有缘无分，来世愿做比翼鸟，双宿双飞！',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 92,
+    headImgSrc: '/images/detail/list/j2.jpg',
+    author: '新华社',
+    dataTime: '24time',
+    attention_count: 88,
+    avatar: '/images/avatar/1.png',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46101.mp3', // 音乐链接
+      title: 'Sunset Jesus-Avicii',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 3
+  },
+  {
+    date: '2018/3/17 下午3:30:35',
+    title: '贾静雯作品展',
+    detail_img: '/images/detail/list/01.jpg',
+    detail_content: '“树欲静而风不止，子欲养而亲不待”，我常常觉得这是世界上最痛彻心扉的悔恨，追悔不及的遗憾，总是在岁月里翻腾起噬人的浪，喘不过气，唯有窒息。羁鸟的牵绊是树木，风筝的归宿总是线，无论飞得多高多远，家永远是最温馨的牵挂，日渐苍老却依然美丽的父母永远是我们生命中天和地一样的存在，也是在这茫茫碧海，朗朗青天之中，我一生的挚爱。',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    headImgSrc: '/images/detail/list/j3.jpg',
+    author: '新华社',
+    dataTime: '24time',
+    avatar: '/images/avatar/2.png',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46102.mp3', // 音乐链接
+      title: '汪峰 - 儿时',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 4
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '娱乐新闻',
+    detail_img: '/images/detail/list/02.jpg',
+    avatar: '/images/avatar/3.png',
+    detail_content: '一季寒暑，一轮春秋，时光红了芍药，绿了芭蕉，却老了双亲。他们的步伐不再矫健，双手不再有力，容颜不再年轻，只有疼爱、牵挂子女的心，从来未曾改变，哪怕时间风化了记忆，掩埋了一切，那种爱，依然强烈如昔。',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/detail/list/j4.jpg',
+    author: '李白3',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '并可以在手机上体验该小程序的实际效果。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46110.mp3', // 音乐链接
+      title: '曲婉婷 - 我的歌声里-(电视剧《在线爱》主题曲)',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 5
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '社会活动',
+    detail_img: '/images/detail/list/03.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '碧海青天，繁华无数，情之万种，各有千秋。花前月下，翩蝶恋花，纵然浪漫美好，亦抵不过父母给予我们的各种温馨，虽然平淡，却融入骨髓，成为不可替代的存在；高山流水，知音之遇，纵然珍稀可贵，亦抵不过这一生只有一次，一次只能一个的父母双亲，前不可寻，后不可追，失去了，便是永远难以弥合的伤痛。千金难买，此生不换，挚爱双亲，是我永远的心灵归宿。',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/detail/list/j6.jpg',
+    author: '新华社',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '并可以在手机上体验该小程序的实际效果。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 6
+  },
+];
+
+module.exports = {list_data};
